0ee3995618960d3db0c839cae249e2ae34093cb1
Ryan-OLoughlin <ryanol2010@hotmail.com>
2025-11-07 15:09:44 +0000

    Added greeting module for different languages

---
+++ node-lab1/greeting.js
@@ -0,0 +1,9 @@
+const greeting = {
+    en: "Hello",
+    fr: "Bonjour",
+    es: "Hola",
+    de: "Guten Tag",
+    it: "Ciao"
+};
+
+export default greeting;
--- node-lab1/index.js
+++ node-lab1/index.js
@@ -1,15 +1,31 @@
 // Load the http module to create an http server.
 import http from 'http';
 import dotenv from 'dotenv';
+import greeting from './greeting.js';
 
 dotenv.config();
-
 const port = process.env.PORT;
-// Configure our HTTP server to respond with Hello World to all requests.
+
+// Configure our HTTP server to receive requests and send responses
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('Gooooooood Morning Vietnam!');
+    // Check headers for language preference
+    let lang = req.headers['accept-language'];
+    const defaultLang = 'en';
+    // If language is not in greeting file, use default
+    if(!greeting[lang]) lang=defaultLang;
+    // Prepare response
+    const response={
+        lang: lang,
+        message: greeting[lang],
+    };
+    // Add headers
+    res.writeHead(200, {
+        'Content-Type': 'text/plain',
+        'Content-Language': response.lang});
+    // End response and send back
+    res.end(response.message);
 });
+
 
 server.listen(port);
 
